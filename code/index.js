fa8f3c99ee4b55188eab4c3c125cc0e2c3d6385c
Nathan-W-2 <nwilliams2@Carthage.edu>
2026-04-03 17:27:55 -0500

    Add minor improvements
    - Unauthorized access will now redirect to login
    - Added path.join for better path handling
    - Balls now clear correctly on clientside

--- code/index.js
+++ code/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
+const path = require('path');
 const http = require('http');         // needed for websockets
 const WebSocket = require('ws');      // needed for websockets
 
@@ -56,20 +57,21 @@ const requiresLogin = (req, res, next) => {
     if (req.session.user) {
         next();
     } else {
-        res.status(401).send("You must be logged in to access this page.");
+        // res.status(401).sendFile(path.join(__dirname, 'public','unauthorized.html'));
+        res.redirect('/login')
     }
 };
 
 app.get('/admin', requiresLogin, (req, res) => {
-  res.sendFile(__dirname + '/public/admin.html');
+  res.status(200).sendFile(__dirname + '/public/admin.html');
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/public/login.html');
+  res.status(200).sendFile(path.join(__dirname, 'public','login.html'));
 });
 
 app.get('/logged-in', (req, res) => {
-  res.sendFile(__dirname + '/public/logged-in.html');
+  res.status(200).sendFile(__dirname + '/public/logged-in.html');
 });
 
 app.post('/register', async (req, res) => {
